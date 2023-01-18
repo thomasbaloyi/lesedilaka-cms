31c08285f2f8b80419d9afe0ed69ee9b2674f318
Thomas Baloyi <baloyi.thomas@outlook.com>
2023-01-18 22:29:31 +0200

    Initial commit

---
+++ src/test/java/com/lesedilaka/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesedilaka.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
